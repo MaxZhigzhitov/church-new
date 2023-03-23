d12bb4f2e5a738af08efed3b2cc8ca74e3a3ddb9
MaxZhigzhitov <maxzhidesktop@gmail.com>
2023-03-23 21:30:35 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.parish__slider-container', {
+    direction: 'horizontal',
+    loop: 'true',
+    slidesPerView: 1,
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+
+})

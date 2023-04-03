8da673d04a8fd8871d7cf8c57c0df10915ecf322
MaxZhigzhitov <maxzhidesktop@gmail.com>
2023-04-03 21:04:52 +0800

    finished adaptive layout; finished header and footer; added interactivity to interface

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
+const headerSidenav = document.querySelector('.header__sidenav');
+const headerSidenavBtn = document.querySelector('.btn-sidenav-toggle');
+
+function headerSidenavToggle(){
+    window.addEventListener('keydown', function(e){
+        if(e.key == 'Escape'){
+            console.log(e.key);
+            headerSidenav.classList.remove('sidenav__open');
+
+        }
+    });
+    
+    if(!headerSidenav.classList.contains('sidenav__open')){
+        headerSidenav.classList.add('sidenav__open');
+    }else{
+        headerSidenav.classList.remove('sidenav__open');
+    }
+}
+
+headerSidenavBtn.addEventListener('click', headerSidenavToggle);
+
+
+
 const swiper = new Swiper('.parish__slider-container', {
     direction: 'horizontal',
     loop: 'true',
     slidesPerView: 1,
+    autoHeight: true,
 
     pagination: {
         el: '.swiper-pagination',
